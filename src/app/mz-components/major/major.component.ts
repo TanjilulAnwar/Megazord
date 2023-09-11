59ba44a86933fc25f012d2ad3462796cb9c44f44
tanjilulanwar <tanjilulanwargmail.com>
2023-09-12 03:55:50 +0600

    parallax added

--- src/app/mz-components/major/major.component.ts
+++ src/app/mz-components/major/major.component.ts
@@ -2,7 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { gsap } from "gsap";
 import { Draggable } from "gsap/Draggable";
 import ScrollTrigger from "gsap/ScrollTrigger";
+
 gsap.registerPlugin(ScrollTrigger, Draggable);
+
+
+
+
 
 @Component({
   selector: 'app-major',
@@ -16,15 +21,99 @@ export class MajorComponent implements OnInit {
   }
 
   setupGsap(): void {
-
+// bismillah part
     gsap.timeline()
-    .fromTo(".msg1", { y: 10, opacity: 0}, {opacity: 1, y: 1, duration: 1})
+    .fromTo(".msg1", { y: 10, opacity: 0}, {opacity: 1, y: 1, duration: 0.5})
     .fromTo(".msg2", { y: 10, opacity: 0}, {opacity: 1, y: 1, duration: 0.5})
     .fromTo(".msg3", { y: 10, opacity: 0}, {opacity: 1, y: 1, duration: 0.5})
-    
+   
+//
 
 
-    
 
+//mandala part
+    gsap.timeline({
+      scrollTrigger:{
+       // trigger: ".mandala-img'",
+        pin: true,
+        scrub:1,
+        start: 'top top',
+        end:'+=10000',
+      }
+    })
+    .to('.mandala-img', {
+      rotation:360*6,
+      duration:1, ease:'none',
+    })
+
+
+    //
+
+//parallax
+/* ------Great Horned Owl Sequence------  */
+gsap.set(".circle", { yPercent: -5});
+gsap.set(".dotsBlue", { yPercent: 10});
+gsap.set(".owlHorned", { yPercent: 0});
+gsap.set(".clusterGreat", { yPercent: 5});
+
+gsap.to(".circle", {
+  yPercent: 5,
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".clusterGreat",
+    scrub: 1
+  }, 
+});
+
+gsap.to(".dotsBlue", {
+  yPercent: -10,
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".clusterGreat",
+    scrub: 1
+  }, 
+});
+
+
+gsap.to(".owlHorned", {
+  yPercent: 0,
+  xPercent:-10,
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".clusterGreat",
+    scrub: 1
+  }, 
+});
+
+gsap.to(".caption", {
+  yPercent: 100,
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".clusterGreat",
+  // markers:true,
+    end: "bottom center",
+    scrub: 1
+  }, 
+});
+
+gsap.to(".clusterGreat", {
+  yPercent: -5,
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".clusterGreat",
+    end: "bottom center",
+    scrub: 1
+  }, 
+});
+
+
+
+//paRALLAX
+
+
+   
   }
+
+
+
 }
