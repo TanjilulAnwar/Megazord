d10f64b916075007c58b5353170acd90b021e853
tanjilulanwar <tanjilulanwar@gmail.com>
2023-09-26 14:32:35 +0600

    routing removed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes =[
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-   { path: 'home',          component: HomeComponent },
-   { path: 'home/:guestId',          component: HomeComponent },
-   { path: ':guestId',          component: HomeComponent },
+  { path: '',  pathMatch: 'full', component:HomeComponent },
+  //  { path: 'home',          component: HomeComponent },
+  //  { path: 'home/:guestId',          component: HomeComponent },
+  //  { path: ':guestId',          component: HomeComponent },
   // { path: 'component',       component: ComponentsComponent },
   // { path: 'user-profile',     component: ProfileComponent },
   // { path: 'signup',           component: SignupComponent },
