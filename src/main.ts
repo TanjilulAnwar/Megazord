197ac2cc94ee63e9f91e7a1df6369f74966c47b3
tanjilulanwar <tanjilulanwargmail.com>
2023-10-24 04:47:15 +0600

    Name server

--- src/main.ts
+++ src/main.ts
@@ -7,3 +7,7 @@ import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+
+  //nia.ns.cloudflare.com
+  //guss.ns.cloudflare.com
